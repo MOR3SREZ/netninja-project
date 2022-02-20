823bedff26da6500284c88832b9aec35426e51d0
mores <morteza78rezaeian@gmail.com>
2022-02-20 23:03:29 +0330

    #22

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Redirect,
+  Navigate,
+} from 'react-router-dom';
 
 // pages & components
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              path='/'
+              element={user ? <Home /> : <Navigate to={'/login'} />}
+            />
 
-          <Route path='/login' element={<Login />} />
+            <Route
+              path='/login'
+              element={user ? <Navigate to={'/'} /> : <Login />}
+            />
 
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+            <Route
+              path='/signup'
+              element={user ? <Navigate to={'/'} /> : <Signup />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,15 @@
-// styles
-import styles from './Home.module.css'
+//components
+import TransactionForm from './TransactionForm';
 
+// styles
+import styles from './Home.module.css';
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.content}>content</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
     </div>
-  )
+  );
 }
